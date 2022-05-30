145c1d81d1e4b14cf16e7e7fe329b18282c75a29
Beer Inder Singh <BeerInder.Singh@techtorium.ac.nz>
2022-05-30 23:29:20 +1200

    Add project files.

---
+++ Project_Music_Cognitive/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Project_Music_Cognitive
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_Username = new System.Windows.Forms.TextBox();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.pb_Captcha = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Refresh_Captcha = new System.Windows.Forms.Button();
+            this.btn_Register = new System.Windows.Forms.Button();
+            this.tb_Privacy_Policy = new System.Windows.Forms.TextBox();
+            this.lbl_PrivacyPolicy = new System.Windows.Forms.Label();
+            this.cb_Privacy_Policy = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_Captcha = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Captcha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(102, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // tb_Username
+            // 
+            this.tb_Username.Location = new System.Drawing.Point(384, 81);
+            this.tb_Username.Name = "tb_Username";
+            this.tb_Username.Size = new System.Drawing.Size(191, 22);
+            this.tb_Username.TabIndex = 2;
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.Location = new System.Drawing.Point(384, 149);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.Size = new System.Drawing.Size(191, 22);
+            this.tb_Password.TabIndex = 3;
+            // 
+            // pb_Captcha
+            // 
+            this.pb_Captcha.Location = new System.Drawing.Point(384, 206);
+            this.pb_Captcha.Name = "pb_Captcha";
+            this.pb_Captcha.Size = new System.Drawing.Size(191, 45);
+            this.pb_Captcha.TabIndex = 4;
+            this.pb_Captcha.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(102, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Captcha";
+            // 
+            // btn_Refresh_Captcha
+            // 
+            this.btn_Refresh_Captcha.Location = new System.Drawing.Point(600, 206);
+            this.btn_Refresh_Captcha.Name = "btn_Refresh_Captcha";
+            this.btn_Refresh_Captcha.Size = new System.Drawing.Size(116, 45);
+            this.btn_Refresh_Captcha.TabIndex = 6;
+            this.btn_Refresh_Captcha.Text = "Refresh";
+            this.btn_Refresh_Captcha.UseVisualStyleBackColor = true;
+            this.btn_Refresh_Captcha.Click += new System.EventHandler(this.btn_Refresh_Captcha_Click);
+            // 
+            // btn_Register
+            // 
+            this.btn_Register.Location = new System.Drawing.Point(337, 505);
+            this.btn_Register.Name = "btn_Register";
+            this.btn_Register.Size = new System.Drawing.Size(178, 51);
+            this.btn_Register.TabIndex = 7;
+            this.btn_Register.Text = "Register";
+            this.btn_Register.UseVisualStyleBackColor = true;
+            this.btn_Register.Click += new System.EventHandler(this.btn_Register_Click);
+            // 
+            // tb_Privacy_Policy
+            // 
+            this.tb_Privacy_Policy.Location = new System.Drawing.Point(384, 353);
+            this.tb_Privacy_Policy.Multiline = true;
+            this.tb_Privacy_Policy.Name = "tb_Privacy_Policy";
+            this.tb_Privacy_Policy.Size = new System.Drawing.Size(191, 110);
+            this.tb_Privacy_Policy.TabIndex = 8;
+            // 
+            // lbl_PrivacyPolicy
+            // 
+            this.lbl_PrivacyPolicy.AutoSize = true;
+            this.lbl_PrivacyPolicy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_PrivacyPolicy.Location = new System.Drawing.Point(98, 393);
+            this.lbl_PrivacyPolicy.Name = "lbl_PrivacyPolicy";
+            this.lbl_PrivacyPolicy.Size = new System.Drawing.Size(133, 25);
+            this.lbl_PrivacyPolicy.TabIndex = 9;
+            this.lbl_PrivacyPolicy.Text = "Privacy Policy";
+            // 
+            // cb_Privacy_Policy
+            // 
+            this.cb_Privacy_Policy.AutoSize = true;
+            this.cb_Privacy_Policy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_Privacy_Policy.Location = new System.Drawing.Point(626, 393);
+            this.cb_Privacy_Policy.Name = "cb_Privacy_Policy";
+            this.cb_Privacy_Policy.Size = new System.Drawing.Size(18, 17);
+            this.cb_Privacy_Policy.TabIndex = 10;
+            this.cb_Privacy_Policy.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(331, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 31);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Register";
+            // 
+            // tb_Captcha
+            // 
+            this.tb_Captcha.Location = new System.Drawing.Point(384, 290);
+            this.tb_Captcha.Name = "tb_Captcha";
+            this.tb_Captcha.Size = new System.Drawing.Size(191, 22);
+            this.tb_Captcha.TabIndex = 12;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 610);
+            this.Controls.Add(this.tb_Captcha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cb_Privacy_Policy);
+            this.Controls.Add(this.lbl_PrivacyPolicy);
+            this.Controls.Add(this.tb_Privacy_Policy);
+            this.Controls.Add(this.btn_Register);
+            this.Controls.Add(this.btn_Refresh_Captcha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pb_Captcha);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.tb_Username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Registration";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Captcha)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_Username;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.PictureBox pb_Captcha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Refresh_Captcha;
+        private System.Windows.Forms.Button btn_Register;
+        private System.Windows.Forms.TextBox tb_Privacy_Policy;
+        private System.Windows.Forms.Label lbl_PrivacyPolicy;
+        private System.Windows.Forms.CheckBox cb_Privacy_Policy;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_Captcha;
+    }
+}
+
---
+++ Project_Music_Cognitive/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Music_Cognitive
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            loadCaptchaImage();
+            tb_Privacy_Policy.Text = "This is a Privacy Policy This is a Privacy Policy This is a Privacy Policy ";
+        }
+        int captcha_num = 0;
+
+        private void loadCaptchaImage()
+        {
+            Random ran = new Random();
+            captcha_num = ran.Next(1000, 10000);
+            var image = new Bitmap(this.pb_Captcha.Width, this.pb_Captcha.Height);
+            var font = new Font("TimesNewRoman", 25, FontStyle.Bold, GraphicsUnit.Pixel);
+            var graphics = Graphics.FromImage(image);
+            graphics.DrawString(captcha_num.ToString(), font, Brushes.Green, new Point(0, 0));
+            pb_Captcha.Image = image;
+        }
+
+        private void btn_Refresh_Captcha_Click(object sender, EventArgs e)
+        {
+            loadCaptchaImage();
+        }
+
+        private void btn_Register_Click(object sender, EventArgs e)
+        {
+            if(tb_Captcha.Text == captcha_num.ToString())
+            {
+                MessageBox.Show("Valid Captcha");
+            }
+            else
+            {
+                MessageBox.Show("Try Again !!!");
+            }
+            if(!cb_Privacy_Policy.Checked)
+            {
+                MessageBox.Show("Read Privacy Policy");
+            }
+            loadCaptchaImage();
+        }
+    }
+}
